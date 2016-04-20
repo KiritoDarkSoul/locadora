338c6683335d52d7802109a706c0f79c52ce09ca
aluno <aluno@micro000>
2016-04-19 21:40:58 -0300

    Adicionado layouts para filmes

---
+++ src/AppBundle/Controller/FilmesController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class FilmesController extends Controller
+{    
+    /**
+     * @Route("/filmes", name="filmes_index")
+     */
+    public function indexAction()
+    {
+        return $this->render('filmes/index.html.twig');
+    }
+    
+    /**
+     * @Route("/filmes/Cdastro")
+     */
+    public function CadastroAction()
+    {
+        return $this->render('filmes/cadastro.html.twig');
+    }
+}
